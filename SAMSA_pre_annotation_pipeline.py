4f0e70b37fcd4828b8ccfcb6c231a5f21fb55005
Sam Westreich <swestreich@dnanexus.com>
2018-02-27 11:21:37 -0800

    Pre-annotation pipeline update for better paired-end function

--- SAMSA_pre_annotation_pipeline.py
+++ SAMSA_pre_annotation_pipeline.py
@@ -23,10 +23,10 @@
 # Created by Sam Westreich, stwestreich@ucdavis.edu, github.com/transcript/
 #
 ##########################################################################
-# 
-# This is a python wrapper script, designed to run all steps of the SAMSA 
+#
+# This is a python wrapper script, designed to run all steps of the SAMSA
 # pipeline up to the MG-RAST annotation step.
-# (Why not the entire pipeline?  Because MG-RAST takes several days to 
+# (Why not the entire pipeline?  Because MG-RAST takes several days to
 # perform the annotation step.)
 #
 # This wrapper performs the following steps:
@@ -43,18 +43,18 @@
 # USAGE OPTIONS
 #
 # REQUIRED:
-# -Ends (#)			Distinguishes between paired or single end data.  
+# -Ends (#)			Distinguishes between paired or single end data.
 #					1 = single end, 2 = paired end.
-# -A (string)		Authorization key for MG-RAST.  Found under "Account Preferences" 
+# -A (string)		Authorization key for MG-RAST.  Found under "Account Preferences"
 #					at www.metagenomics.anl.gov/ .
 # -D (path)			Path to folder containing the raw sequence files to be processed.
 #
 # OPTIONAL:
-# -Q				Activates quiet mode; comments and updates are not printed to 
+# -Q				Activates quiet mode; comments and updates are not printed to
 #					STDOUT.
-# -T (path)			Absolute location (path) of 'trimmomatic-0.33.jar', if it isn't located 
+# -T (path)			Absolute location (path) of 'trimmomatic-0.33.jar', if it isn't located
 #					in /Applications/Trimmomatic-0.33/trimmomatic-0.33.jar .
-# -F (path) 		Location (path) of 'flash', if it isn't located in 
+# -F (path) 		Location (path) of 'flash', if it isn't located in
 #					/Applications/FLASH-1.2.11/flash* .
 #
 ##########################################################################
@@ -78,7 +78,7 @@ if "-Q" in argv:
 	quiet = True
 else:
 	quiet = False
-	
+
 # Opening statement/disclaimer
 if quiet == False:
 	print ("This is part 1 of the SAMSA pipeline, handling preprocessing and uploading of all chosen files to MG-RAST for annotation.")
@@ -94,13 +94,13 @@ if "-USAGE" in argv:
 	print ("-Ends (#)\tDistinguishes between paired or single end data; 1=SE, 2=PE.")
 	print ("-A (string)\tAuthorization key for MG-RAST.  Found under 'Account preferences' at www.metagenomics.anl.gov/.")
 	print ("-D (path)\tFolder containing raw sequence files to be processed.")
-	
+
 	print ("\nOPTIONAL:")
 	print ("-Q\tActivates quiet mode; comments and updates are not printed to STDOUT.")
 	print ("-T\tSpecifies location of Trimmomatic .jar file (trimmomatic-0.33.jar) if not installed in standard /Applications/Trimmomatic-0.33/trimmomatic-0.33.jar location.")
-	print ("-F\tSpecifies location of flash program file if not installed in standard /Applications/FLASH-1.2.11/flash location.")
+	print ("-F\tSpecifies location of flash program file if not installed in standard /Applications/FLASH-1.2.11/flash location.\n")
 	sys.exit()
-	
+
 # Checking to see if required options are included
 if "-ENDS" not in argv:
 	print ("FATAL ERROR:\nType of sequencing (paired or single end) not specified with '-Ends' flag.  Please see '-usage' for required arguments when running this wrapper script.\nTERMINATED")
@@ -111,6 +111,10 @@ elif "-A" not in argv:
 elif "-D" not in argv:
 	print ("FATAL ERROR:\nLocation of metatranscriptome sequence files not specified with '-D' flag.  Please see '-usage' for required arguments when running this script.\nTERMINATED")
 	sys.exit
+else:
+	print "Ends type:\t" + string_find("-ENDS")
+	print "Auth key:\t" + string_find("-A")
+	print "FIles location:\t" + string_find("-D")
 
 # Checking for Trimmomatic
 if quiet == False:
@@ -127,7 +131,7 @@ if "does not exist" in T_check:
 	if quiet == False:
 		if "-T" in argv:
 			print ("\nFATAL ERROR:\nTrimmomatic not found at location " + custom_T_location + ".  Please download Trimmomatic from http://www.usadellab.org/cms/?page=trimmomatic and install in /Applications/.\nTERMINATED")
-			sys.exit()	
+			sys.exit()
 		else:
 			print ("\nFATAL ERROR:\nTrimmomatic not found at the default location.  Please download Trimmomatic from http://www.usadellab.org/cms/?page=trimmomatic and install in /Applications/, or specify location of .jar file using the -T argument with this script.\nTERMINATED")
 			sys.exit()
@@ -138,14 +142,14 @@ end_type = string_find("-ENDS")
 if end_type == "2":
 	if quiet == False:
 		print ("\nPaired end reads are specified; checking to see if FLASH is installed...")
-	
+
 	# check for FLASH aligner program
 	if "-F" in argv:
 		custom_F_location = string_find("-F")
 		F_check = subprocess.check_output("if [ ! -f " + custom_F_location + " ]; then echo \"FLASH does not exist.\"; fi", shell = True)
 	else:
 		F_check = subprocess.check_output("if [ ! -f /Applications/FLASH-1.2.11/flash ]; then echo \"FLASH does not exist.\"; fi", shell = True)
-	
+
 	# terminating if FLASH isn't found
 	if "does not exist" in F_check:
 		if quiet == False:
@@ -158,7 +162,7 @@ if end_type == "2":
 elif end_type == "1":
 	if quiet == False:
 		print ("\nSingle end reads are specified; skipping FLASH alignment step.\n")
-		
+
 # If we've made it this far, we've got both Trimmomatic and FLASH (if FLASH is necessary)!  Now, time to run some actual stuff!
 # Need to echo all files in folder location, and then match them up if they're paired-end files.
 # WARNING: Doesn't work with folders with spaces in their names.
@@ -169,31 +173,57 @@ files_list = subprocess.check_output("ls " + files_location, shell = True).split
 
 # removes last 'empty item' from list
 files_list1 = files_list.pop()
-#print len(files_list)
 
-# Running Trimmomatic on files
-for file in files_list:
-	if end_type == "1":
+# SINGLE END APPROACH
+if end_type == "1":
+	for file in files_list:
 		if "-T" in argv:
-			Trim_command = "java -jar " + custom_T_location + " SE " + files_location + file + " " + files_location + "trimmed_" + file + " ILLUMINACLIP:" + custom_T_location[:-20] + "/adaptors/TruSeq3-SE.fa:2:30:10 MAXINFO:100:0.2"
+			Trim_command = "java -jar " + custom_T_location + " SE " + files_location + file + " " + files_location + "trimmed_" + file + " ILLUMINACLIP:" + custom_T_location[:-20] + "/adapters/TruSeq3-SE.fa:2:30:10 MAXINFO:100:0.2"
 		else:
-			 Trim_command = "java -jar /Applications/Trimmomatic-0.33/trimmomatic-0.33.jar SE " + files_location + file + " " + files_location + "trimmed_" + file + " ILLUMINACLIP:/Applications/Trimmomatic-0.33/adaptors/TruSeq3-SE.fa:2:30:10 MAXINFO:100:0.2"
-	elif end_type == "2":
-		if "-T" in argv:
-			Trim_command = "java -jar " + custom_T_location + " SE " + files_location + file + " " + files_location + "trimmed_" + file + " ILLUMINACLIP:" + custom_T_location[:-20] + "/adaptors/TruSeq3-PE.fa:2:30:10 MAXINFO:100:0.2"
-		else:
-			 Trim_command = "java -jar /Applications/Trimmomatic-0.33/trimmomatic-0.33.jar SE " + files_location + file + " " + files_location + "trimmed_" + file + " ILLUMINACLIP:/Applications/Trimmomatic-0.33/adaptors/TruSeq3-PE.fa:2:30:10 MAXINFO:100:0.2"
+			 Trim_command = "java -jar /Applications/Trimmomatic-0.33/trimmomatic-0.33.jar SE " + files_location + file + " " + files_location + "trimmed_" + file + " ILLUMINACLIP:/Applications/Trimmomatic-0.33/adapters/TruSeq3-SE.fa:2:30:10 MAXINFO:100:0.2"
+		# execute
+		if quiet == False:
+			print ("\nTrimmomatic command used: " + Trim_command)
+		os.system(Trim_command)
 
-	# execute
-	if quiet == False:
-		print ("\nTrimmomatic command used: " + Trim_command)
-	os.system(Trim_command)
+# PAIRED END APPROACH
+elif end_type == "2":
+	forward_dic = {}
+	reverse_dic = {}
+	front_halves = []
+	for file in files_list:
+		if "R" in str(file):
+			string_name = str(file).split("R")
+			first_half = str(string_name[0])
+			second_half = str(string_name[1])
+			if second_half[0] == "1":
+				forward_dic[first_half] = file
+			elif second_half[0] == "2":
+				reverse_dic[first_half] = file
+				if first_half in forward_dic.keys():			# checking to make sure we also have a forward file
+					front_halves.append(first_half)
+	for file_prefix in front_halves:
+		if file_prefix in forward_dic:
+			if file_prefix in reverse_dic:
+				# if we've reached this point, there's a forward and reverse file; time for FLASH
+				forward_file = forward_dic[file_prefix]
+				reverse_file = reverse_dic[file_prefix]
+				print forward_file, reverse_file
+				output_names = [file_prefix + "R1_paired_trimmed.fastq", file_prefix + "R1_unpaired_trimmed.fastq", file_prefix + "R2_paired_trimmed.fastq", file_prefix + "R2_unpaired_trimmed.fastq"]
+				if "-T" in argv:
+					Trim_command = "java -jar " + custom_T_location + " PE " + files_location + forward_file + " " + files_location + reverse_file + " " + " ".join(output_names) + " ILLUMINACLIP:" + custom_T_location[:-20] + "/adapters/TruSeq3-PE.fa:2:30:10 MAXINFO:100:0.2"
+				else:
+					 Trim_command = "java -jar /Applications/Trimmomatic-0.33/trimmomatic-0.33.jar PE " + files_location + forward_file + " " + files_location + reverse_file + " " + " ".join(output_names) + " " + file_prefix + " ILLUMINACLIP:/Applications/Trimmomatic-0.33/adapters/TruSeq3-PE.fa:2:30:10 MAXINFO:100:0.2"
+				# execute
+				if quiet == False:
+					print ("\nTrimmomatic command used: " + Trim_command)
+				os.system(Trim_command)
 
 # getting the list of trimmed files only
 files_list = subprocess.check_output("ls " + files_location, shell = True).split("\n")
 trimmed_files_list = []
 for file in files_list:
-	if "trimmed" in file:
+	if "_paired_trimmed" in file:
 		trimmed_files_list.append(file)
 
 # Running FLASH on files (only if paired-end)
@@ -202,45 +232,50 @@ if end_type == "2":
 	reverse_dic = {}
 	front_halves = []
 	for file in trimmed_files_list:
-		string_name = str(file).split("R")
-		first_half = str(string_name[0])
-		second_half = str(string_name[1])
-		if second_half[0] == "1":
-			forward_dic[first_half] = file 
-		elif second_half[0] == "2":
-			reverse_dic[first_half] = file
-			front_halves.append(first_half)
-	
+		if "R" in str(file):
+			string_name = str(file).split("R")
+			first_half = str(string_name[0])
+			second_half = str(string_name[1])
+			if second_half[0] == "1":
+				forward_dic[first_half] = file
+			elif second_half[0] == "2":
+				reverse_dic[first_half] = file
+				# check to make sure that we also have R1
+				if first_half in forward_dic.keys():
+					front_halves.append(first_half)
+
 	# we should now have 2 dictionaries, one with the forward files, one with the reverse files
 	# now we just need to match them up!
-	for file_pair in front_halves:
-		if file_pair in forward_dic:
-			if file_pair in reverse_dic:
-				# if we've reached this point, there's a forward and reverse file
-				# time for FLASH!
-				
+	for file_prefix in front_halves:
+		if file_prefix in forward_dic:
+			if file_prefix in reverse_dic:
+				# if we've reached this point, there's a forward and reverse file; time for FLASH
+				forward_file = forward_dic[file_prefix]
+				reverse_file = reverse_dic[file_prefix]
 				if "-F" in argv:
-					FLASH_command = custom_F_location + " -o " + file_pair + " -d " + string_find("-D") + " " + string_find("-D") + forward_dic[file_pair] + " " + string_find("-D") + reverse_dic[file_pair]
+					FLASH_command = custom_F_location + " -o " + file_prefix[:-1] + " -d " + string_find("-D") + " " + string_find("-D") + forward_file + " " + string_find("-D") + reverse_file
 					if quiet == False:
 						print ("\nFLASH command used: " + FLASH_command)
 				else:
-					FLASH_command = "/Applications/FLASH-1.2.11/flash " + " -o " + file_pair + " -d " + string_find("-D") + string_find("-D") + forward_dic[file_pair] + " " + string_find("-D") + reverse_dic[file_pair]
+					FLASH_command = "/Applications/FLASH-1.2.11/flash " + " -o " + file_prefix[:-1] + " -d " + string_find("-D") + string_find("-D") + forward_file + " " + string_find("-D") + reverse_file
 					if quiet == False:
 						print ("\nFLASH command used: " + FLASH_command)
 				# execute
 				os.system(FLASH_command)
+
+print "Made it up to MG-RAST upload step."
 
 # Next is going to be uploading these files to MG-RAST.
 # We're going to need a few things here: We'll need the MG-RAST authorization key, and the files will be named differently if they were paired end vs. single end.
 if end_type == "1":			# single end files
 	files_list = subprocess.check_output("ls " + files_location + "trimmed_*", shell = True).split("\n")
 	files_list1 = files_list.pop()
-	
+
 	if quiet == False:
 		print ("\nFiles to be uploaded to MG-RAST:")
 		for file in files_list:
 			print file
-	
+
 	# Upload command
 	if quiet == False:
 		for file in files_list:
@@ -254,19 +289,19 @@ if end_type == "1":			# single end files
 elif end_type == "2":		# paired end files
 	files_list = subprocess.check_output("ls " + files_location + "*.extendedFrags*", shell = True).split("\n")
 	files_list1 = files_list.pop()
-	
+
 	if quiet == False:
 		print ("\nFiles to be uploaded to MG-RAST:")
 		for file in files_list:
 			print file
 		print ("\n")
-	
+
 	# Find out where uploader_MG-RAST.py is located
 	if "/" in sys.argv[0]:
 		path = str(sys.argv[0]).split("SAMSA_pre_annotation_pipeline.py")
 	else:
 		path = "./"
-	
+
 	# Upload command
 	if quiet == False:
 		for file in files_list:
@@ -294,7 +329,7 @@ inbox_info_command = 'curl -X GET -H "auth: ' + string_find("-A") + '" "http://a
 print inbox_info_command
 inbox_info = subprocess.check_output(inbox_info_command, shell = True)
 inbox_info_split = inbox_info.split('id":"')
-first_one = inbox_info_split.pop(0) 
+first_one = inbox_info_split.pop(0)
 first_two = inbox_info_split.pop(0)
 inbox_IDs = []
 for item in inbox_info_split:
@@ -309,6 +344,6 @@ for UUID in inbox_IDs:
 	if quiet == False:
 		print ("Command used: " + seq_stats_command)
 	os.system(seq_stats_command)
-	
+
 # Unfortunately, it takes some time for the sequence stats to be computed.  So submission's going to have to be a separate step, performed via the online portal.  See the documentation for more information.
 print ("For next steps, log in to the MG-RAST online portal.  See Step 3 in the documentation for more information.")
